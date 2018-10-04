fad82a9f0e7cbf14f56a2b7bf112ad5070934df0
julianajlk <julianajlk@gmail.com>
2018-10-04 11:25:57 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,27 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  //2. dispatch
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  //1. getState
+  function getState() {
+    return state;
+  }
+
+  //INIT
+  dispatch({ type: "@@INIT" });
+
+  // return an object with 2 methods
+  return {
+    dispatch,
+    getState
+  };
 }
 
 function render() {
-  const container = document.getElementById('container');
+  const container = document.getElementById("container");
 }
